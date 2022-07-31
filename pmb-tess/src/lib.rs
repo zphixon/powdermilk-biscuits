33e74a755da871e777d5ee5a4ddf74c0e2da728d
Zack H <zphixon@gmail.com>
2022-07-31 16:29:10 -0400

    lib: Continue playing with bezier curves

--- pmb-tess/src/lib.rs
+++ pmb-tess/src/lib.rs
@@ -1,14 +1,35 @@
-pub trait Quadratic<P: Point> {
-    fn quadratic(&self) -> Bezier2<P>;
+pub trait ToBezier<P: Point> {
+    fn quadratic(&self) -> Quadratic<P>;
+    fn cubic(&self) -> Cubic<P>;
 }
 
-impl<P: Point> Quadratic<P> for &[P] {
-    fn quadratic(&self) -> Bezier2<P> {
+impl<P: Point, const N: usize> ToBezier<P> for &[P; N] {
+    fn quadratic(&self) -> Quadratic<P> {
+        self.as_slice().quadratic()
+    }
+
+    fn cubic(&self) -> Cubic<P> {
+        self.as_slice().cubic()
+    }
+}
+
+impl<P: Point> ToBezier<P> for &[P] {
+    fn quadratic(&self) -> Quadratic<P> {
         assert_eq!(3, self.len());
-        Bezier2 {
+        Quadratic {
             a: self[0],
             b: self[1],
             c: self[2],
+        }
+    }
+
+    fn cubic(&self) -> Cubic<P> {
+        assert_eq!(4, self.len());
+        Cubic {
+            a: self[0],
+            b: self[1],
+            c: self[2],
+            d: self[3],
         }
     }
 }
@@ -17,49 +38,149 @@ pub trait Point: Clone + Copy {
     fn new(x: f32, y: f32) -> Self;
     fn x(&self) -> f32;
     fn y(&self) -> f32;
+    fn zero() -> Self {
+        Self::new(0., 0.)
+    }
 }
 
-pub struct Bezier2<P: Point> {
+pub trait Bezier<P: Point> {
+    fn weighted_basis(&self, t: f32) -> P;
+    fn casteljau(&self, t: f32) -> P;
+    fn flatten(&self, segments: usize) -> Vec<P> {
+        steps(segments).map(|t| self.casteljau(t)).collect()
+    }
+    fn derivative(&self, t: f32) -> P;
+    fn tangent(&self, t: f32) -> P {
+        self.derivative(t)
+    }
+    fn direction(&self, t: f32) -> P {
+        let tan = self.tangent(t);
+        let d = (tan.x().powi(2) + tan.y().powi(2)).sqrt();
+        P::new(tan.x() / d, tan.y() / d)
+    }
+    fn normal(&self, t: f32) -> P {
+        let dir = self.direction(t);
+        P::new(-dir.y(), dir.x())
+    }
+}
+
+#[derive(Clone)]
+pub struct Quadratic<P: Point> {
     pub a: P,
     pub b: P,
     pub c: P,
 }
 
-pub struct Bezier3<P: Point> {
+#[derive(Clone)]
+pub struct Cubic<P: Point> {
     pub a: P,
     pub b: P,
     pub c: P,
     pub d: P,
 }
 
-macro_rules! interpolate {
-    ($whomst:ty, $which:ident, $($field:ident),* $(,)?) => {
-        impl<P: Point> $whomst  {
-            pub fn interpolate(&self, t: f32) -> P {
-                P::new(
-                    $which(t, $(<P as Point>::x(&self.$field)),*),
-                    $which(t, $(<P as Point>::y(&self.$field)),*),
-                )
-            }
+pub fn steps(steps: usize) -> impl Iterator<Item = f32> {
+    (0..=steps).map(move |t| t as f32 / steps as f32)
+}
+
+impl<P: Point> Bezier<P> for Quadratic<P> {
+    fn weighted_basis(&self, t: f32) -> P {
+        let t2 = t * t;
+        let mt = 1. - t;
+        let mt2 = mt * mt;
+        P::new(
+            mt2 * self.a.x() + 2. * mt * t * self.b.x() + t2 * self.c.x(),
+            mt2 * self.a.y() + 2. * mt * t * self.b.y() + t2 * self.c.y(),
+        )
+    }
+
+    fn casteljau(&self, t: f32) -> P {
+        let tn = 1. - t;
+        let ab_x = self.a.x() * tn + self.b.x() * t;
+        let ab_y = self.a.y() * tn + self.b.y() * t;
+        let bc_x = self.b.x() * tn + self.c.x() * t;
+        let bc_y = self.b.y() * tn + self.c.y() * t;
+        let ab_bc_x = ab_x * tn + bc_x * t;
+        let ab_bc_y = ab_y * tn + bc_y * t;
+        P::new(ab_bc_x, ab_bc_y)
+    }
+
+    fn derivative(&self, t: f32) -> P {
+        let dx = 2. * (1. - t) * (self.b.x() - self.a.x()) + 2. * t * (self.c.x() - self.b.x());
+        let dy = 2. * (1. - t) * (self.b.y() - self.a.y()) + 2. * t * (self.c.y() - self.b.y());
+        P::new(dx, dy)
+    }
+}
+
+impl<P: Point> Cubic<P> {
+    pub fn derivative2(&self, t: f32) -> P {
+        let apx = 3. * (self.b.x() - self.a.x());
+        let apy = 3. * (self.b.y() - self.a.y());
+        let bpx = 3. * (self.c.x() - self.b.x());
+        let bpy = 3. * (self.c.y() - self.b.y());
+        let cpx = 3. * (self.d.x() - self.c.x());
+        let cpy = 3. * (self.d.y() - self.c.y());
+        let ddx = 2. * Self::basis(0, t) * (bpx - apx) + 2. * Self::basis(1, t) * (cpx - bpx);
+        let ddy = 2. * Self::basis(0, t) * (bpy - apy) + 2. * Self::basis(1, t) * (cpy - bpy);
+        P::new(ddx, ddy)
+    }
+
+    fn basis(k: usize, t: f32) -> f32 {
+        assert!((0..=2).contains(&k));
+        if k == 0 {
+            (1. - t) * (1. - t)
+        } else if k == 1 {
+            1. - t
+        } else {
+            t * t
         }
-    };
+    }
 }
 
-interpolate!(Bezier2<P>, bezier2, a, b, c);
-interpolate!(Bezier3<P>, bezier3, a, b, c, d);
+impl<P: Point> Bezier<P> for Cubic<P> {
+    fn weighted_basis(&self, t: f32) -> P {
+        let t2 = t * t;
+        let t3 = t2 * t;
+        let mt = 1. - t;
+        let mt2 = mt * mt;
+        let mt3 = mt2 * mt;
+        P::new(
+            mt3 * self.a.x()
+                + 3. * mt2 * t * self.b.x()
+                + 3. * mt * t2 * self.c.x()
+                + t3 * self.d.x(),
+            mt3 * self.a.y()
+                + 3. * mt2 * t * self.b.y()
+                + 3. * mt * t2 * self.c.y()
+                + t3 * self.d.y(),
+        )
+    }
 
-pub fn bezier2(t: f32, w1: f32, w2: f32, w3: f32) -> f32 {
-    let t2 = t * t;
-    let mt = 1. - t;
-    let mt2 = mt * mt;
-    mt2 * w1 + 2. * mt * t * w2 + t2 * w3
-}
+    fn casteljau(&self, t: f32) -> P {
+        let tn = 1. - t;
+        let ab_x = self.a.x() * tn + self.b.x() * t;
+        let ab_y = self.a.y() * tn + self.b.y() * t;
+        let bc_x = self.b.x() * tn + self.c.x() * t;
+        let bc_y = self.b.y() * tn + self.c.y() * t;
+        let cd_x = self.c.x() * tn + self.d.x() * t;
+        let cd_y = self.c.y() * tn + self.d.y() * t;
+        let ab_bc_x = ab_x * tn + bc_x * t;
+        let ab_bc_y = ab_y * tn + bc_y * t;
+        let bc_cd_x = bc_x * tn + cd_x * t;
+        let bc_cd_y = bc_y * tn + cd_y * t;
+        let x = ab_bc_x * tn + bc_cd_x * t;
+        let y = ab_bc_y * tn + bc_cd_y * t;
+        P::new(x, y)
+    }
 
-pub fn bezier3(t: f32, w1: f32, w2: f32, w3: f32, w4: f32) -> f32 {
-    let t2 = t * t;
-    let t3 = t2 * t;
-    let mt = 1. - t;
-    let mt2 = mt * mt;
-    let mt3 = mt2 * mt;
-    mt3 * w1 + 3. * mt2 * t * w2 + 3. * mt * t2 * w3 + t3 * w4
+    fn derivative(&self, t: f32) -> P {
+        let tn = 1. - t;
+        let dx = 3. * tn * tn * (self.b.x() - self.a.x())
+            + 3. * tn * t * (self.c.x() - self.b.x())
+            + 3. * t * t * (self.d.x() - self.c.x());
+        let dy = 3. * tn * tn * (self.b.y() - self.a.y())
+            + 3. * tn * t * (self.c.y() - self.b.y())
+            + 3. * t * t * (self.d.y() - self.c.y());
+        P::new(dx, dy)
+    }
 }
--- pmb-tess/src/main.rs
+++ pmb-tess/src/main.rs
@@ -1,3 +1,61 @@
+#![allow(dead_code)]
+
+/*
+Pos { x: -3.3346415, y: 4.1671815 },
+Pos { x: -3.4107695, y: 4.1671815 },
+Pos { x: -3.4799757, y: 4.161991 },
+Pos { x: -3.5543728, y: 4.148496 },
+Pos { x: -3.6253088, y: 4.1246195 },
+Pos { x: -3.739501, y: 4.0696 },
+Pos { x: -3.813898, y: 4.018734 },
+Pos { x: -3.8709936, y: 3.9689047 },
+Pos { x: -3.9073267, y: 3.9138858 },
+Pos { x: -3.9678822, y: 3.8007324 },
+Pos { x: -3.9990263, y: 3.7031515 },
+Pos { x: -4.016327, y: 3.6180267 },
+Pos { x: -4.0249777, y: 3.5391312 },
+Pos { x: -4.0249777, y: 3.449855 },
+Pos { x: -4.011136, y: 3.2806442 },
+Pos { x: -3.9903746, y: 3.1197388 },
+Pos { x: -3.9540405, y: 2.9754431 },
+Pos { x: -3.9107876, y: 2.845681 },
+Pos { x: -3.853691, y: 2.7346036 },
+Pos { x: -3.8052464, y: 2.6608994 },
+Pos { x: -3.7533426, y: 2.6079562 },
+Pos { x: -3.7048979, y: 2.5685084 },
+Pos { x: -3.6426115, y: 2.5373654 },
+Pos { x: -3.582056, y: 2.5166037 },
+Pos { x: -3.5197697, y: 2.4999933 },
+Pos { x: -3.4574833, y: 2.4948027 },
+Pos { x: -3.4142294, y: 2.4948027 },
+Pos { x: -3.3796253, y: 2.505184 },
+Pos { x: -3.3623235, y: 2.5238693 },
+Pos { x: -3.353673, y: 2.547746 },
+Pos { x: -3.3571339, y: 2.582003 },
+Pos { x: -3.3830862, y: 2.6484418 },
+Pos { x: -3.4713252, y: 2.8301096 },
+Pos { x: -3.5768652, y: 3.006586 },
+Pos { x: -3.6772146, y: 3.1882539 },
+Pos { x: -3.7879457, y: 3.3730352 },
+Pos { x: -3.888294, y: 3.5526266 },
+Pos { x: -3.976533, y: 3.732218 },
+Pos { x: -4.0301685, y: 3.8630183 },
+Pos { x: -4.0699625, y: 3.9979713 },
+Pos { x: -4.090724, y: 4.095553 },
+Pos { x: -4.095914, y: 4.1775627 },
+Pos { x: -4.087264, y: 4.237772 },
+Pos { x: -4.0595818, y: 4.2990203 },
+Pos { x: -3.9851847, y: 4.4069824 },
+Pos { x: -3.8502314, y: 4.535707 },
+Pos { x: -3.6910563, y: 4.643669 },
+Pos { x: -3.511118, y: 4.7412505 },
+Pos { x: -3.3830862, y: 4.7973084 },
+Pos { x: -3.2515926, y: 4.855442 },
+Pos { x: -3.120101, y: 4.910461 },
+ */
+
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
 use pmb_tess::*;
 
 #[derive(Clone, Copy, Debug)]
@@ -20,31 +78,69 @@ impl Point for Pos {
     }
 }
 
+impl Display for Pos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{:.05},{:.05}", self.x, self.y)
+    }
+}
+
+fn as_storkeelment(int: Pos) {
+    println!(
+        "        StrokeElement{{x:{},y:{},pressure:1.}},",
+        int.x, int.y
+    );
+}
+
+#[rustfmt::skip]
+fn print(c: &[Pos], bez: &impl Bezier<Pos>) {
+    println!("\n\n\n\nimport matplotlib.pyplot as plt\nfig,ax=plt.subplots()\nax.grid(True)");
+    print!("xp=["); c.iter().for_each(|p| print!("{:.05},", p.x())); println!("]");
+    print!("yp=["); c.iter().for_each(|p| print!("{:.05},", p.y())); println!("]");
+
+    let steps: Vec<Pos> = bez.flatten(32);
+    print!("xc=["); steps.iter().for_each(|p| print!("{:.05},", p.x())); println!("]");
+    print!("yc=["); steps.iter().for_each(|p| print!("{:.05},", p.y())); println!("]");
+
+    let steps: Vec<Pos> = pmb_tess::steps(32).map(|t| bez.casteljau(t)).collect();
+    print!("xw=["); steps.iter().for_each(|p| print!("{:.05},", p.x())); println!("]");
+    print!("yw=["); steps.iter().for_each(|p| print!("{:.05},", p.y())); println!("]");
+
+    let steps: Vec<Pos> = pmb_tess::steps(32).map(|t| bez.derivative(t)).collect();
+    print!("xd=["); steps.iter().for_each(|p| print!("{:.05},", p.x())); println!("]");
+    print!("yd=["); steps.iter().for_each(|p| print!("{:.05},", p.y())); println!("]");
+
+    let steps: Vec<Pos> = pmb_tess::steps(32).map(|t| bez.direction(t)).collect();
+    print!("xdd=["); steps.iter().for_each(|p| print!("{:.05},", p.x())); println!("]");
+    print!("ydd=["); steps.iter().for_each(|p| print!("{:.05},", p.y())); println!("]");
+
+    let steps: Vec<Pos> = pmb_tess::steps(32).map(|t| {
+        let p = bez.casteljau(t);
+        let n = bez.normal(t);
+        Pos { x: p.x + n.x, y: p.y + n.y }
+    }).collect();
+    print!("xn=["); steps.iter().for_each(|p| print!("{:.05},", p.x())); println!("]");
+    print!("yn=["); steps.iter().for_each(|p| print!("{:.05},", p.y())); println!("]");
+
+    println!("ax.plot(xp, yp, c='black')");
+    println!("ax.plot(xc, yc, c='firebrick')");
+    println!("ax.plot(xw, yc, c='lightcoral')");
+    println!("ax.plot(xd, yd, c='seagreen')");
+    println!("ax.plot(xdd, ydd, c='palegreen')");
+    println!("ax.plot(xn, yn, c='darkorchid')");
+    println!("plt.show()\n\n\n\n");
+}
+
 fn main() {
     #[rustfmt::skip]
     let points = &[
         Pos { x: 8.148584, y: -3.3291578 },
-        Pos { x: 8.148584, y: -3.3291578 },
-        Pos { x: 8.461885, y: -3.1091926 },
-        Pos { x: 9.155146, y: -2.269322 },
-        Pos { x: 10.121714, y: -0.50959253 },
-        Pos { x: 10.555004, y: 0.776875 },
-        Pos { x: 11.094946, y: 2.5899289 },
-        Pos { x: 11.094946, y: 2.5899289 },
+        Pos { x: -9.155146, y: -2.269322 },
+        Pos { x: 10.121714, y: 0.50959253 },
+        Pos { x: 11.094946, y: 8.5899289 },
     ];
+    let cub = points.cubic();
+    let quad = (&points[1..]).quadratic();
 
-    points.windows(3).for_each(|w| {
-        let quad = w.quadratic();
-
-        let steps = 10;
-        for t in (0..=steps).map(|t| t as f32 / steps as f32) {
-            let int = quad.interpolate(t);
-            println!(
-                "        StrokeElement{{x:{},y:{},pressure:1.}},",
-                int.x, int.y
-            );
-        }
-
-        println!();
-    });
+    print(points, &cub);
+    print(points, &quad);
 }
